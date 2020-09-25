a76786f2513166c119be7d9925746d492f8c17b1
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-09-25 12:43:07 +0100

    feat: add type checks

--- flip.js
+++ flip.js
@@ -4,4 +4,18 @@ const flip = (f, n = 2, m = 0) => (...a) => f(
   ...a.filter((x, i) => i >= n + m),
 );
 
-module.exports = flip;
+module.exports = (f, n = 2, m = 0) => {
+  if (typeof f !== 'function') {
+    throw new TypeError(`Expected function but recieved ${typeof f}.`);
+  } 
+
+  if (typeof n !== 'number' || isNaN(n)) {
+    throw new TypeError(`Expected number but recieved ${typeof n}.`);
+  }
+
+  if (typeof m !== 'number' || isNaN(m)) {
+    throw new TypeError(`Expected number but recieved ${typeof m}.`);
+  }
+
+  return flip(f, n, m);
+};
